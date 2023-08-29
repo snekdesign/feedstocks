f63ca5b419adc99d7d0dba56546eb90c70659111
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-08-30 01:20:02 +0800

    aria2-toolchain: use coreutils inside SRC_DIR

--- aria2-toolchain/bld.bat
+++ aria2-toolchain/bld.bat
@@ -1,14 +1,5 @@
 @echo off
-set MSYSTEM=UCRT64
-cd msys64 || exit /b 1
-usr\bin\bash -l "%CD%\..\scripts\1.sh"
-usr\bin\bash -l "%CD%\..\scripts\2.sh" || exit /b 1
-rd /S /Q home || exit /b 1
-mklink /H usr\lib\librt.a ..\msys2-runtime-devel\usr\lib\librt.a || exit /b 1
-
-robocopy . "%LIBRARY_PREFIX%" *.* /E
-rem https://ss64.com/nt/robocopy-exit.html
-if %ERRORLEVEL% GEQ 8 (
-    exit /b 1
-)
-exit /b 0
+msys64\usr\bin\bash -l "%CD%\scripts\1.sh"
+msys64\usr\bin\bash -l "%CD%\scripts\2.sh" ^
+    && msys64\usr\bin\rm -r msys64\home ^
+    && msys64\usr\bin\cp -alT msys64 "%LIBRARY_PREFIX%"
