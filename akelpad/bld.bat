cf4aeec9c26b377615aeb09050d87436aa3697f5
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-09-23 18:43:58 +0800

    akelpad: add missing akelpad_4

--- akelpad/bld.bat
+++ akelpad/bld.bat
@@ -1,6 +1,6 @@
 @echo off
-md build ^
-    && cd build ^
+md akelpad_4\build ^
+    && cd akelpad_4\build ^
     && cmake ^
         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
         -G"Visual Studio 17 2022" ^
