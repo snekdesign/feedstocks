01978a7df441461ce5825ff9382821a6cedecb11
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-09-25 18:55:21 +0800

    akelpad: SRC_DIR should the parent of trunk

--- akelpad/bld.bat
+++ akelpad/bld.bat
@@ -1,6 +1,6 @@
 @echo off
-md akelpad_4\build ^
-    && cd akelpad_4\build ^
+md trunk\akelpad_4\build ^
+    && cd trunk\akelpad_4\build ^
     && cmake ^
         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
         -G"Visual Studio 17 2022" ^
