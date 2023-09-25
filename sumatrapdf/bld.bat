a0547db370a7e28ebdb47f56059a08b03e71c8e1
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-09-25 19:15:20 +0800

    sumatrapdf: mkdir etc

--- sumatrapdf/bld.bat
+++ sumatrapdf/bld.bat
@@ -1,4 +1,5 @@
 @echo off
-mklink /H "%LIBRARY_PREFIX%\etc\SumatraPDF-settings.txt" SumatraPDF-settings.txt ^
+md "%LIBRARY_PREFIX%\etc" ^
+    && mklink /H "%LIBRARY_PREFIX%\etc\SumatraPDF-settings.txt" SumatraPDF-settings.txt ^
     && msbuild vs2022\SumatraPDF.vcxproj -p:Configuration=Release;Platform=x64 ^
     && mklink /H "%LIBRARY_BIN%\SumatraPDF.exe" out\rel64\SumatraPDF.exe
