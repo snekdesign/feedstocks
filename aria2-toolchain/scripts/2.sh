c22d62357e7fe9a25e0d0323f1c0afaf24981710
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-10-03 09:10:59 +0800

    aria2-toolchain: remove unused dependencies

--- aria2-toolchain/scripts/2.sh
+++ aria2-toolchain/scripts/2.sh
@@ -3,10 +3,7 @@ set -e
 pacman -Su --noconfirm
 pacman -S --noconfirm \
     $MINGW_PACKAGE_PREFIX-autotools \
-    $MINGW_PACKAGE_PREFIX-c-ares \
-    $MINGW_PACKAGE_PREFIX-cppunit \
     $MINGW_PACKAGE_PREFIX-gcc \
-    $MINGW_PACKAGE_PREFIX-libssh2 \
     $MINGW_PACKAGE_PREFIX-sqlite3
 pacman -Scc --noconfirm
 touch /dev/mqueue/.empty /dev/shm/.empty /tmp/.empty
