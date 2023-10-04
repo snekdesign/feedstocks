2bc104ffd27c254063a9931038b60bbc2b73fb3a
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-10-04 13:24:41 +0800

    7zz: flatten

--- 7zz/bld.bat
+++ 7zz/bld.bat
@@ -1,5 +1,6 @@
 @echo off
-cd CPP\7zip\Bundles\Alone2 ^
+mklink /H License.txt DOC\License.txt ^
+    && cd CPP\7zip\Bundles\Alone2 ^
     && nmake MY_DYNAMIC_LINK=1 PLATFORM=x64 ^
     && mklink /H "%LIBRARY_BIN%\7zz.exe" x64\7zz.exe ^
     && cd ..\SFXWin ^
