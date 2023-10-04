51f1c1fc5c05d50b615c9cf135b7429b7ac41e96
snekdesign <104812819+snekdesign@users.noreply.github.com>
2023-10-04 13:34:29 +0800

    sumatrapdf: copy settings directly

--- sumatrapdf/bld.bat
+++ sumatrapdf/bld.bat
@@ -1,5 +1,5 @@
 @echo off
 md "%LIBRARY_PREFIX%\etc" ^
-    && mklink /H "%LIBRARY_PREFIX%\etc\SumatraPDF-settings.txt" SumatraPDF-settings.txt ^
+    && copy "%RECIPE_DIR%\SumatraPDF-settings.txt" "%LIBRARY_PREFIX%\etc" ^
     && msbuild vs2022\SumatraPDF.vcxproj -p:Configuration=Release;Platform=x64 ^
     && mklink /H "%LIBRARY_BIN%\SumatraPDF.exe" out\rel64\SumatraPDF.exe
