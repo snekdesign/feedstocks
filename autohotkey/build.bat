8b49653ec14e1eb43d6a56f87c549cea6e5f6dcf
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-06-11 21:27:27 +0800

    autohotkey: add spy.ico

--- autohotkey/build.bat
+++ autohotkey/build.bat
@@ -1,6 +1,7 @@
 @echo off
-md "%LIBRARY_PREFIX%\etc" && ^
+md "%LIBRARY_PREFIX%\etc\inc" && ^
 msbuild AutoHotkeyx.vcxproj -p:Configuration=Release;Platform=x64 && ^
 mklink /H "%LIBRARY_BIN%\AutoHotkey.exe" bin\AutoHotkey64.exe && ^
 mklink /H "%LIBRARY_PREFIX%\etc\AutoHotkey.chm" bin\AutoHotkey.chm && ^
-mklink /H "%LIBRARY_PREFIX%\etc\WindowSpy.ahk" bin\UX\WindowSpy.ahk
+mklink /H "%LIBRARY_PREFIX%\etc\WindowSpy.ahk" bin\UX\WindowSpy.ahk && ^
+mklink /H "%LIBRARY_PREFIX%\etc\inc\spy.ico" bin\UX\inc\spy.ico
