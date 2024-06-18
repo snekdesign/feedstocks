2e46c020a0eb5e38515e190435e10c60e6fdc493
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-06-18 19:15:48 +0800

    powershell: version 7.4.2

---
+++ powershell/build.ps1
@@ -0,0 +1,21 @@
+#Requires -Version 7.4
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+Set-StrictMode -Version Latest
+
+$v = [Environment]::Version
+$r = ".store\powershell\$Env:PKG_VERSION\powershell\$Env:PKG_VERSION\tools\net$($v.Major).$($v.Minor)\any"
+$d = "$Env:DOTNET_TOOLS\$r"
+$n = 'Microsoft.PowerShell.GlobalTool.Shim'
+mkdir $d
+@"
+with open('$n.exe', 'rb') as f:
+    b = f.read()
+with open(r'$Env:DOTNET_TOOLS\pwsh.exe', 'x+b') as f:
+    f.write(b)
+    f.seek(b.index(b'$n.dll' + b'\0'*($($r.Length)+1)))
+    f.write(br'$r\$n.dll')
+"@ | python -
+cp.exe -al "$n.dll" "$n.runtimeconfig.json" $d
+cp.exe -al . "$d\win"
+Remove-Item "$d\win\*.bat"
