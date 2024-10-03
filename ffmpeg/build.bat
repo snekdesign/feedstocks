46541101fc2c5a69053ad77abefdac7be717dc22
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-10-04 07:00:37 +0800

    ffmpeg: version 7.1.0

--- ffmpeg/build.bat
+++ ffmpeg/build.bat
@@ -1,2 +1,6 @@
 @echo off
-bash -el "%RECIPE_DIR%\build.sh"
+bash -el "%RECIPE_DIR%\build.sh" && ^
+cd /d "%LIBRARY_PREFIX%"
+if %ERRORLEVEL% neq 0 exit 1
+rm -rf dev etc home usr/share/info/dir || ^
+break
