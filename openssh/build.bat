867666596457c2e3372758262dc54da4ae8ddec6
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-11-10 21:49:27 +0800

    openssh: remove duplicates from patch file

--- openssh/build.bat
+++ openssh/build.bat
@@ -1,5 +1,35 @@
 @echo off
 cd contrib\win32\openssh && ^
+sed -i ^
+    -e "s MultiThreaded< MultiThreadedDLL< " ^
+    -e "s %%(PreprocessorDefinitions) HAVE_STRNLEN;%%(PreprocessorDefinitions) " ^
+    -e "s >Spectre< >false< " ^
+    config.vcxproj ^
+    keygen.vcxproj ^
+    libssh.vcxproj ^
+    openbsd_compat.vcxproj ^
+    scp.vcxproj ^
+    sftp-server.vcxproj ^
+    sftp.vcxproj ^
+    ssh-add.vcxproj ^
+    ssh-agent.vcxproj ^
+    ssh-keyscan.vcxproj ^
+    ssh-pkcs11-helper.vcxproj ^
+    ssh-shellhost.vcxproj ^
+    ssh-sk-helper.vcxproj ^
+    ssh.vcxproj ^
+    sshd-session.vcxproj ^
+    sshd.vcxproj ^
+    unittest-bitmap.vcxproj ^
+    unittest-hostkeys.vcxproj ^
+    unittest-kex.vcxproj ^
+    unittest-match.vcxproj ^
+    unittest-misc.vcxproj ^
+    unittest-sshbuf.vcxproj ^
+    unittest-sshkey.vcxproj ^
+    unittest-utf8.vcxproj ^
+    unittest-win32compat.vcxproj ^
+    win32iocompat.vcxproj && ^
 msbuild -p:Configuration=Release;Platform=x64 Win32-OpenSSH.sln && ^
 mklink /H "%LIBRARY_BIN%\libcrypto.dll" LibreSSL\bin\desktop\X64\libcrypto.dll && ^
 copy "%SRC_DIR%\bin\x64\Release\*.exe" "%LIBRARY_BIN%" && ^
