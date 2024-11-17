7fd63fee41b5cf4a0fe36ab3d346235417aacc19
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-11-17 19:45:15 +0800

    nsudodm: reduce binary size

--- nsudodm/build.bat
+++ nsudodm/build.bat
@@ -1,3 +1,9 @@
 @echo off
-msbuild Source\Native\NSudoDevilMode\NSudoDevilMode.vcxproj -p:Configuration=Release;Platform=x64 && ^
-mklink /H "%LIBRARY_BIN%\NSudoDM.dll" Source\Native\Output\Binaries\Release\x64\NSudoDM.dll
+cd Source\Native\NSudoDevilMode && ^
+cl /GL -I..\MINT /LD /MD /O2 ^
+    NSudoDevilMode.cpp ^
+    detours.cpp ^
+    disasm.cpp ^
+    kernel32.lib ^
+    "%LIBRARY_PREFIX%\x86_64-w64-mingw32\sysroot\usr\lib\libntoskrnl.a" && ^
+mklink /H "%LIBRARY_BIN%\NSudoDM.dll" NSudoDevilMode.dll
