0bacd0b890688189b531705626e1da38fbbef831
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-11-24 16:25:33 +0800

    vscodium: exclude conda_build.bat

--- vscodium/build.bat
+++ vscodium/build.bat
@@ -1,4 +1,4 @@
 @echo off
-md "%LIBRARY_BIN%" "%LIBRARY_PREFIX%\opt\vscodium" && ^
-cp -al . "%LIBRARY_PREFIX%\opt\vscodium" && ^
+md "%LIBRARY_BIN%" "%LIBRARY_PREFIX%\opt" && ^
+cp -al vscodium "%LIBRARY_PREFIX%\opt" && ^
 copy "%RECIPE_DIR%\code.cmd" "%LIBRARY_BIN%"
