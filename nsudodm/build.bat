5947cbe9679a1e97ae7dbc8647f304ac974bebbb
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-02-05 21:31:05 +0800

    nsudodm: latest msvc compat

--- nsudodm/build.bat
+++ nsudodm/build.bat
@@ -5,6 +5,7 @@ cl /GL -I..\MINT /LD /MD /O2 ^
     detours.cpp ^
     disasm.cpp ^
     kernel32.lib ^
+    ntdll.lib ^
     "%LIBRARY_PREFIX%\x86_64-w64-mingw32\sysroot\usr\lib\libntoskrnl.a" && ^
 mklink /H "%LIBRARY_BIN%\NSudoDM.dll" NSudoDevilMode.dll && ^
 md "%PREFIX%\etc\conda" && ^
