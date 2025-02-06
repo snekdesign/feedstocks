92dd6f8af74843aabcd63e8883909231a572e52a
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-02-06 08:24:57 +0800

    nginx: version 1.27.4

--- nginx/build.sh
+++ nginx/build.sh
@@ -2,14 +2,14 @@ cd "$SRC_DIR"
 ./configure \
     --conf-path=etc/nginx/nginx.conf \
     --error-log-path=var/log/nginx/error.log \
-    --http-client-body-temp-path=var/tmp/nginx/client \
-    --http-fastcgi-temp-path=var/tmp/nginx/fastcgi \
+    --http-client-body-temp-path=var/lib/nginx/client \
+    --http-fastcgi-temp-path=var/lib/nginx/fastcgi \
     --http-log-path=var/log/nginx/access.log \
-    --http-proxy-temp-path=var/tmp/nginx/proxy \
-    --http-scgi-temp-path=var/tmp/nginx/scgi \
-    --http-uwsgi-temp-path=var/tmp/nginx/uwsgi \
-    --lock-path=var/run/nginx/nginx.lock \
-    --pid-path=var/run/nginx/nginx.pid \
+    --http-proxy-temp-path=var/lib/nginx/proxy \
+    --http-scgi-temp-path=var/lib/nginx/scgi \
+    --http-uwsgi-temp-path=var/lib/nginx/uwsgi \
+    --lock-path=run/lock/nginx.lock \
+    --pid-path=run/nginx.pid \
     --prefix='E:' \
     --with-cc=cl \
     --with-http_ssl_module \
