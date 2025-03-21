65d156daec75a93b87a85514df050ffe77ab87d6
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-03-21 21:32:09 +0800

    pixi-build-backends: version 0.1.4

--- pixi-build-backends/build.bat
+++ pixi-build-backends/build.bat
@@ -1,6 +1,22 @@
 @echo off
 if EXIST "%LIBRARY_BIN%\pixi-build-cmake.exe" ( exit 0 )
 cargo install ^
+    --locked ^
     --no-track ^
-    --path pixi-build-backends/crates/pixi-build ^
+    --path pixi-build-backends/crates/pixi-build-cmake ^
+    --root "%LIBRARY_PREFIX%" && ^
+cargo install ^
+    --locked ^
+    --no-track ^
+    --path pixi-build-backends/crates/pixi-build-python ^
+    --root "%LIBRARY_PREFIX%" && ^
+cargo install ^
+    --locked ^
+    --no-track ^
+    --path pixi-build-backends/crates/pixi-build-rattler-build ^
+    --root "%LIBRARY_PREFIX%" && ^
+cargo install ^
+    --locked ^
+    --no-track ^
+    --path pixi-build-backends/crates/pixi-build-rust ^
     --root "%LIBRARY_PREFIX%"
--- pixi-build-backends/build.sh
+++ pixi-build-backends/build.sh
@@ -2,6 +2,22 @@ if [ -f "$PREFIX/bin/pixi-build-cmake" ]; then
     exit 0
 fi
 cargo install \
+    --locked \
     --no-track \
-    --path pixi-build-backends/crates/pixi-build \
+    --path pixi-build-backends/crates/pixi-build-cmake \
+    --root "$PREFIX"
+cargo install \
+    --locked \
+    --no-track \
+    --path pixi-build-backends/crates/pixi-build-python \
+    --root "$PREFIX"
+cargo install \
+    --locked \
+    --no-track \
+    --path pixi-build-backends/crates/pixi-build-rattler-build \
+    --root "$PREFIX"
+cargo install \
+    --locked \
+    --no-track \
+    --path pixi-build-backends/crates/pixi-build-rust \
     --root "$PREFIX"
