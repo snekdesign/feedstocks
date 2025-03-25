025915c815dfb37719fe28b18c29dd1fbc23f8aa
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-03-26 06:06:29 +0800

    cmd: version 0.4.15 release

--- cmd/build.bat
+++ cmd/build.bat
@@ -1,5 +1,5 @@
 @echo off
-call configure.cmd %CMAKE_ARGS%
-cd output-VS-amd64
-ninja base\shell\cmd\cmd.exe
+call configure.cmd %CMAKE_ARGS% && ^
+cd output-VS-amd64 && ^
+ninja base\shell\cmd\cmd.exe && ^
 mklink /H "%LIBRARY_BIN%\cmd.exe" base\shell\cmd\cmd.exe
