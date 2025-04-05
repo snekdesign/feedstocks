1f89bf944adfbd7dd0cea61fc62fde329b7053bf
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-04-05 09:19:00 +0800

    pixi, uv: use msvc link on windows

--- pixi/build.bat
+++ pixi/build.bat
@@ -1,6 +1,11 @@
 @echo off
-for %%G in ("%RECIPE_DIR%") do set "CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_LINKER=%%~dpGrust\link.cmd"
 set "VC_LTL=%SRC_DIR%\vc_ltl"
 set "YY_THUNKS=%SRC_DIR%\yy_thunks"
 7z x -ovc_ltl VC-LTL-Binary.7z
-cargo install --locked --no-track --path pixi --profile dist --root "%LIBRARY_PREFIX%"
+cargo install ^
+    --config target.x86_64-pc-windows-msvc.linker=\"link\" ^
+    --locked ^
+    --no-track ^
+    --path pixi ^
+    --profile dist ^
+    --root "%LIBRARY_PREFIX%"
