46c8c3482023affdec56d1ed7079d2c9ac1849ce
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-04-13 09:06:33 +0800

    powershell: version 7.5.0

--- powershell/build.ps1
+++ powershell/build.ps1
@@ -3,18 +3,8 @@ Set-StrictMode -Version Latest
 Set-Variable ErrorActionPreference, PSNativeCommandUseErrorActionPreference 1
 
 $v = [Environment]::Version
-$r = ".store\powershell\$Env:PKG_VERSION\powershell\$Env:PKG_VERSION\tools\net$($v.Major).$($v.Minor)\any"
-$d = "$Env:DOTNET_TOOLS\$r"
-$n = 'Microsoft.PowerShell.GlobalTool.Shim'
+$d = "$Env:DOTNET_TOOLS\.store\powershell\$Env:PKG_VERSION\powershell\$Env:PKG_VERSION\tools\net$($v.Major).$($v.Minor)\any\win"
 mkdir $d
-@"
-with open('$n.exe', 'rb') as f:
-    b = f.read()
-with open(r'$Env:DOTNET_TOOLS\pwsh.exe', 'x+b') as f:
-    f.write(b)
-    f.seek(b.index(b'$n.dll' + b'\0'*($($r.Length)+1)))
-    f.write(br'$r\$n.dll')
-"@ | python -
-cp.exe -al "$n.dll" "$n.runtimeconfig.json" $d
-cp.exe -al . "$d\win"
-Remove-Item "$d\win\*.bat"
+cp.exe -al . $d
+Set-Location $d
+Remove-Item *.bat, *.xml
