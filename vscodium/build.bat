ac0566a6f5e6ce20b9e1b0cbe631b3debd38e207
snekdesign <104812819+snekdesign@users.noreply.github.com>
2025-06-13 19:36:39 +0800

    vscodium: version 1.101.03933

--- vscodium/build.bat
+++ vscodium/build.bat
@@ -1,4 +1,3 @@
 @echo off
-md "%LIBRARY_BIN%" "%LIBRARY_PREFIX%\opt" && ^
-cp -al vscodium "%LIBRARY_PREFIX%\opt" && ^
-copy "%RECIPE_DIR%\code.cmd" "%LIBRARY_BIN%"
+md "%LIBRARY_PREFIX%\opt" && ^
+cp -al vscodium "%LIBRARY_PREFIX%\opt"
--- vscodium/code.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-call "%~dp0..\opt\vscodium\bin\codium.cmd" --disable-extension vscode.objective-c %*
