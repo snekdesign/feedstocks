2401d3fa8a050e27a8f6b9d400bebd18ec884e1b
snekdesign <104812819+snekdesign@users.noreply.github.com>
2026-03-17 22:12:46 +0800

    ffmpeg: version 8.1.0

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -1,15 +1,30 @@
 # name conflict
 SUBDIR=
 
-cd "$SRC_DIR"
-mkdir -p build
-cd build
-CFLAGS="$CFLAGS -MD" \
-    PKG_CONFIG_PATH="$LIBRARY_LIB\pkgconfig" \
+mkdir -p "$SRC_DIR/build"
+cd "$_"
+LDFLAGS="$LDFLAGS -fuse-ld=lld" \
+    PKG_CONFIG_PATH="$LIBRARY_LIB\\pkgconfig" \
     ../configure \
+    --ar="$AR" \
+    --cc="$CC" \
+    --cpu=host \
+    --cxx="$CXX" \
+    --disable-avx512 \
     --disable-debug \
+    --disable-fma4 \
+    --disable-runtime-cpudetect \
+    --disable-xop \
     --enable-gpl \
+    --enable-libplacebo \
+    --enable-libshaderc \
+    --enable-lto \
     --enable-shared \
+    --host-extralibs= \
+    --ld="$CC" \
+    --nm="$NM" \
+    --ranlib="$RANLIB" \
+    --strip=llvm-strip \
     --toolchain=msvc
-make -j`nproc`
+make -j$CPU_COUNT
 ln -t "$LIBRARY_BIN" lib*/*-*.dll ffmpeg.exe ffplay.exe ffprobe.exe
